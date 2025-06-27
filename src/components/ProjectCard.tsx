004e8c1cc1d4edea08e7d24231c5cb1f9ce7a9bc
AkhatorEnosa <aosakhogba@gmail.com>
2025-06-27 14:57:39 +0100

    project update

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -17,11 +17,12 @@ const ProjectCard = ({ title, desc, tech, url, construction, imgSrc }: ProjectCa
       whileInView={{ opacity: 1 }}
       transition={{ duration: 0.5, ease: "easeInOut" }}
       viewport={{ amount: 0.2 }}
+      aria-label={title}
       href={url} target="_blank" className="group card relative text-sm bg-[#1f2937]/5 dark:bg-[#f9fafb]/5 md:bg-transparent md:darK:bg-transparent md:hover:bg-[#1f2937]/5 md:dark:hover:bg-[#f9fafb]/5 dark:text-[#f9fafb] rounded-lg p-4 transition-all duration-300 flex flex-col gap-2">
         <div className="flex flex-col-reverse md:grid md:grid-cols-5 justify-between items-center gap-4 md:gap-6">
             <div className="w-full md:w-fit md:col-span-3 md:h-full flex flex-col justify-between gap-2">
                 <div className="w-full flex flex-col gap-2">
-                  <h2 className="w-full flex gap-2 text-lg font-bold group-hover:text-[#008080] group-hover:dark:text-inherit capitalize">{title} <i className="bi bi-arrow-right -rotate-45 group-hover:rotate-0 transition-all duration-300"></i></h2>
+                  <h2 aria-label={title} className="w-full flex gap-2 text-lg font-bold group-hover:text-[#008080] group-hover:dark:text-inherit capitalize">{title} <i className="bi bi-arrow-right -rotate-45 group-hover:rotate-0 transition-all duration-300"></i></h2>
                   <p className="text-sm">{desc}</p>
                 </div>
                 {/* <hr className="text-[#1f2937]/20"/> */}
--- src/sections/Left.tsx
+++ src/sections/Left.tsx
@@ -74,14 +74,16 @@ const Left = () => {
           }
         </div>
 
-        <motion.div
+        <motion.button
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.5, delay: 2.5}}
-          className="fixed right-2 top-5 lg:relative lg:right-0 lg:top-0 w-fit flex bg-[#e9edf1]/80 dark:bg-[#2a3b52]/80 dark:text-[#f9fafb]/60 dark:hover:text-[#f9fafb] hover:font-semibold py-2 px-4 rounded-full backdrop-blur-sm shadow-lg lg:shadow-none lg:backdrop-blur-none cursor-pointer overflow-hidden z-40 duration-150 transition-all" aria-label={theme === 'dark' ? 'Switch to light mode' : 'Switch to dark mode'} onClick={() => themeHandler()}>
-          <p className={`w-fit text-sm flex justify-center items-center gap-2 ${theme === 'dark' ? "-translate-x-30 opacity-0" : "translate-x-0 opacity-100"} duration-150 transition-all`}>Light Mode<i className={`text-2xl bi bi-brightness-high-fill text-orange-300 ${theme !== 'dark' ? "rotate-180" : "rotate-0"} duration-150 transition-all`}></i></p>
-          <p className={`absolute w-fit text-sm flex justify-center items-center gap-2 ${theme !== 'dark' ? "-translate-x-20 opacity-0" : "translate-x-0 opacity-100"} duration-150 transition-all`}>Dark Mode<i className={`text-xl bi bi-moon-fill text-yellow-200 ${theme === 'dark' ? "rotate-[360deg]" : "rotate-0"} duration-150 transition-all`}></i></p>
-        </motion.div>
+          className="fixed right-2 top-5 lg:relative lg:right-0 lg:top-0 w-fit flex bg-[#e9edf1]/80 dark:bg-[#2a3b52]/80 dark:text-[#f9fafb]/60 dark:hover:text-[#f9fafb] hover:font-semibold py-2 px-4 rounded-full backdrop-blur-sm shadow-lg lg:shadow-none lg:backdrop-blur-none cursor-pointer overflow-hidden z-40 duration-150 transition-all" 
+          aria-label={theme === 'dark' ? 'Switch to light mode' : 'Switch to dark mode'} 
+          onClick={() => themeHandler()}>
+            <p className={`w-fit text-sm flex justify-center items-center gap-2 ${theme === 'dark' ? "-translate-x-30 opacity-0" : "translate-x-0 opacity-100"} duration-150 transition-all`}>Light Mode<i className={`text-2xl bi bi-brightness-high-fill text-orange-300 ${theme !== 'dark' ? "rotate-180" : "rotate-0"} duration-150 transition-all`}></i></p>
+            <p className={`absolute w-fit text-sm flex justify-center items-center gap-2 ${theme !== 'dark' ? "-translate-x-20 opacity-0" : "translate-x-0 opacity-100"} duration-150 transition-all`}>Dark Mode<i className={`text-xl bi bi-moon-fill text-yellow-200 ${theme === 'dark' ? "rotate-[360deg]" : "rotate-0"} duration-150 transition-all`}></i></p>
+        </motion.button>
       </motion.div>
     </section>
   )
