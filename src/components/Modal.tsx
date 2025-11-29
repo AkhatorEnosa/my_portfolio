912ff74bebfe71712af8387122db53408819334e
AkhatorEnosa <aosakhogba@gmail.com>
2025-11-29 21:53:32 +0100

    UIUX update

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -21,11 +21,14 @@ const Modal = ({ imgSrc, alt, index } : { imgSrc: string, alt: string, index: nu
 
         <motion.div
             key={index}
-            className={show ? "fixed top-0 left-0 flex w-screen h-screen text-white justify-center items-center z-50 cursor-pointer" : "relative group h-52 overflow-clip rounded-md z-20 break-words"}
+            className={show ? "fixed top-0 left-0 flex w-screen h-screen justify-center items-center z-50 cursor-pointer" : "relative group h-52 overflow-clip rounded-md z-20 break-words"}
             onClick={() => setShow(!show)}
         >
             <div className={show ? "absolute top-0 left-0 h-full w-full bg-[#f9fafb]/80 dark:bg-[#2a3b52]/80 backdrop-blur-sm z-10" : "absolute opacity-0 group-hover:opacity-100 h-full w-full bg-[#f9fafb]/50 dark:bg-[#2a3b52]/50 flex justify-center items-center top-0 left-0 z-50"}>
                 <i className={`bi bi-eye ${show && "hidden"} size-5 p-4 hover:p-6 animate-bounce bg-[#f9fafb] dark:bg-[#2a3b52] rounded-full text-center flex justify-center items-center cursor-pointer transition-all duration-150`}></i>
+            </div>
+            <div aria-label="Close modal" onClick={() => setShow(false)} className={`${!show && "hidden"} absolute top-5 right-5 px-2 py-1 text-xs dark:bg-[#f9fafb] bg-[#2a3b52] text-[#f9fafb] dark:text-[#2a3b52] rounded-full text-center flex justify-center items-center gap-1 font-semibold cursor-pointer transition-all duration-150 z-50`}>
+                <i className={`bi bi-x-lg text-red-600`}></i> Close
             </div>
             <motion.img
                 src={imgSrc} 
@@ -34,7 +37,7 @@ const Modal = ({ imgSrc, alt, index } : { imgSrc: string, alt: string, index: nu
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5, delay: index * 0.2 }}
                 viewport={{ amount: 0.2 }}
-                className={show ? "md:h-[100vh] z-40" : `w-fit group-hover:cursor-pointer scale-50 transition-all duration-300 z-20`}
+                className={show ? "lg:h-[100vh] z-40" : `w-fit group-hover:cursor-pointer scale-50 transition-all duration-300 z-20`}
             />
         </motion.div>
     )
--- src/components/TabButton.tsx
+++ src/components/TabButton.tsx
@@ -17,7 +17,7 @@ const TabButton = ({ title, index } : { title: string, index: number }) => {
     }
   return (
     <button
-      className={`relative w-fit flex justify-center items-center capitalize text-xs gap-3 ${groupBy === title ? "font-semibold text-[#f9fafb] hover:text-[#f9fafb] dark:text-[#1f2937] dark:hover:text-[#1f2937]" : " bg-[#1f2937]/5 dark:bg-[#1f2937]/80 hover:text-[#008080] dark:text-[#f9fafb]/60 dark:hover:text-[#f9fafb] hover:shadow-sm"} py-2 px-4 overflow-clip rounded-full transition-all duration-300`}
+      className={`relative w-fit flex justify-center items-center capitalize text-nowrap text-xs gap-3 ${groupBy === title ? "font-semibold text-[#f9fafb] hover:text-[#f9fafb] dark:text-[#1f2937] dark:hover:text-[#1f2937]" : " bg-[#1f2937]/5 dark:bg-[#1f2937]/80 hover:text-[#008080] dark:text-[#f9fafb]/60 dark:hover:text-[#f9fafb] hover:shadow-sm"} py-2 px-4 overflow-clip rounded-full transition-all duration-300`}
       aria-label={`Group by ${title}`}
       role="button"
       onClick={() => {
@@ -26,6 +26,7 @@ const TabButton = ({ title, index } : { title: string, index: number }) => {
       }}
     >
       {/* <span className={`absolute ${groupBy === title ? "opacity-100 left-0" : "opacity-0 -left-20"} h-full w-full top-0 bg-[#1f2937] dark:bg-[#f9fafb] rounded-full transition-all duration-300 z-0`}></span> */}
+
       <span className={groupBy === title ? `absolute opacity-100 left-0 h-full w-full top-0 bg-[#1f2937] dark:bg-[#f9fafb] rounded-full transition-all duration-300 z-0` : `absolute opacity-0 ${index > tabIndex ? "-left-20" : "left-20" } h-full w-full top-0 bg-[#1f2937] dark:bg-[#f9fafb] rounded-full transition-all duration-300 z-0`}></span>
       <i className='relative not-italic z-10'>{title}</i>
     </button>
